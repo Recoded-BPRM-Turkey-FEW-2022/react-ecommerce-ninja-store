c390604cf7796c711131c6be543217e21e2d7146
mustafa Rast <mularas78@gmail.com>
2022-06-24 23:31:53 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "./style.css";
-<<<<<<< HEAD
 import { useQuery } from "react-query";
 import ProductPage from './components/ProductPage'
 import Button from '@mui/material/Button';
 
 
-=======
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './components/NavbarComp';
 // import MainPage from "./components/MainPage.jsx";  
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { QueryClientProvider,QueryClient } from 'react-query';
->>>>>>> 37eed78f06276a552e98673c3fd49f9a10465f66
 
 import Price from "./components/FilterPrice";
 import MainPage from "./components/NewMain"
@@ -27,16 +24,9 @@ const queryClient = new QueryClient();
 export default function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <ProductPage/>
-=======
-      
-
-      {/* <MainPage/> */}
 
        <QueryClientProvider client={queryClient}> 
-      <BrowserRouter>
-      
+      <BrowserRouter>  
       <Routes>
 
       <Route path='/' element={ <NewMain />} />
@@ -44,13 +34,11 @@ export default function App() {
       <Route path='/Test/:id' element={ <Test />} />
 
       <Route path='/FakeCart' element={ <FakeCart />} />
-      
       </Routes>
       </BrowserRouter>
    
       <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
->>>>>>> 37eed78f06276a552e98673c3fd49f9a10465f66
     </div>
   );
 }
